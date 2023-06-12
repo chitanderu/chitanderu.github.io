ef46e14b8e3899341d5d96a7037561c1e31763f9
chitanderu <982653387@qq.com>
2023-06-13 03:22:15 +0800

    Site updated: 2023-06-13 03:22:15

---
+++ musicrss/music1.js
@@ -0,0 +1,10 @@
+
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    audio: [{
+        name: '君に吹く風',
+        artist: '下川美娜',
+        url: '君に吹く風.mp3',
+        cover: 'https://pic.imgdb.cn/item/64395b5c0d2dde577725723e.jpg '
+    }]
+});
