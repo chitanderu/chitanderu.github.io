335e172b2d15113bd540010489bda6014936ae45
chitanderu <982653387@qq.com>
2023-06-13 03:27:09 +0800

    Site updated: 2023-06-13 03:27:09

--- musicrss/music1.js
+++ musicrss/music1.js
@@ -1,10 +1,11 @@
 
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
+    fixed: true,
     audio: [{
         name: '君に吹く風',
         artist: '下川美娜',
-        url: '君に吹く風.mp3',
+        url: 'https://cdn.jsdelivr.net/gh/chitanderu/asoul2d@latest/%E5%90%9B%E3%81%AB%E5%90%B9%E3%81%8F%E9%A2%A8.mp3',
         cover: 'https://pic.imgdb.cn/item/64395b5c0d2dde577725723e.jpg '
     }]
 });
